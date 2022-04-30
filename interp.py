19aa93b7c1fa73fa14fd56b6bdfe30e344aaa32b
Zihao Xu <zihao.xu@columbia.edu>
2022-04-30 12:05:04 +0800

    linear interp 1d

--- interp.py
+++ interp.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import tensorflow_probability as tfp
 
 class KNN():
     def __init__(self, points, values, k=3):
@@ -20,7 +21,7 @@ class KNN():
     def __call__(self, *args, **kwargs):
         return self.interp(*args, **kwargs)
         
-    def L2_dist2(self, pop1, pop2):
+    def _L2_dist2(self, pop1, pop2):
         """
         pop1 : tensor-like with shape (N, D)
         pop2 : tensor-like with shape (M, D)
@@ -38,7 +39,7 @@ class KNN():
         """
         points = tf.convert_to_tensor(points, dtype=tf.float32)
         
-        dr2 = - self.L2_dist2(points, self.ref_points)
+        dr2 = - self._L2_dist2(points, self.ref_points)
         dr2, ind = tf.math.top_k(dr2, self.k)
         weights = 1.0 / tf.clip_by_value(tf.sqrt(-dr2), 1e-6, float('inf'))
         vals = tf.gather(self.ref_values, ind)
@@ -49,11 +50,11 @@ class KNN():
 class NN_LinearGrid():
     def __init__(self, values, binning):
         """
-        values  : tensor-like with shape (ax1_n_bin, ..., axD_n_bin)
+        values  : tensor-like with shape (ax1_n_points, ..., axD_n_points)
         binning : [
-            [ax1_lower, ax1_upper, ax1_n_bin], 
+            [ax1_lower, ax1_upper, ax1_n_points], 
             ...,
-            [axD_lower, axD_upper, axD_n_bin], 
+            [axD_lower, axD_upper, axD_n_points], 
             ]
             
         The algorithm is the same as the one in straxen with k=2^dim (https://github.com/XENONnT/straxen/blob/a8402c46053105ad5c60b38ae91ef9b8a1571aa7/straxen/itp_map.py#L15).
@@ -103,3 +104,80 @@ class NN_LinearGrid():
         vals = tf.gather_nd(self.ref_values, ind)
         vals = tf.math.reduce_sum(vals * weights, axis=0) / tf.math.reduce_sum(weights, axis=0)
         return vals
+    
+    
+class Linear1D():
+    def __init__(self, points, values):
+        """
+        points : tensor-like with shape (N,), as the reference points
+        values : tensor-like with shape (N,), as the values at reference points
+        """
+        self.ref_points = tf.convert_to_tensor(points, dtype=tf.float32)
+        self.ref_values = tf.convert_to_tensor(values, dtype=tf.float32)
+        self.num_ref = len(points)
+        
+        order = tf.argsort(self.ref_points)
+        self.ref_points = tf.gather(self.ref_points, order)
+        self.ref_values = tf.gather(self.ref_values, order)
+        
+        assert self.num_ref > 2, "Number of reference points must be larger than 2!"
+        
+    def __call__(self, *args, **kwargs):
+        return self.interp(*args, **kwargs)
+        
+    def _dist(self, pop1, pop2):
+        """
+        pop1 : tensor-like with shape (N,)
+        pop2 : tensor-like with shape (M,)
+        
+        return : L2 distance squared with shape (N, M)
+        """
+        dr = tf.expand_dims(pop1, axis=1) - tf.expand_dims(pop2, axis=0)
+        return dr
+        
+    def interp(self, points):
+        """
+        points : tensor-like with shape (N,), as the points to be interpolated.
+        
+        return : linearly interpolated values at the points with shape (N,). Extration is clipped by the boundary values. 
+        """
+        points = tf.convert_to_tensor(points, dtype=tf.float32)
+        
+        dr = self._dist(points, self.ref_points)
+        ind = tf.reduce_sum(tf.cast(dr>=0, dtype=tf.int32), axis=-1) # find right ref point
+        ind = tf.stack((ind-1, ind), axis=1) # stack with left ref point
+        ind = tf.clip_by_value(ind, 0, self.num_ref-1) # clip if outside
+        
+        vals = tf.gather(self.ref_values, ind)
+        rpos = tf.gather(self.ref_points, ind)
+        
+        x0 = rpos[:, 0]
+        x1 = rpos[:, 1]
+        y0 = vals[:, 0]
+        y1 = vals[:, 1]
+        dx = x1 - x0 # dx could be zero if extrap
+        y = ((x1 - points) * y0 + (points - x0) * y1) / dx
+        
+        return tf.where(dx==0, tf.gather(self.ref_values, ind[:, 0]), y)
+    
+    
+class Linear1D_LinearGrid():
+    def __init__(self, values, binning):
+        """
+        values  : tensor-like with shape (N,), as the reference values
+        binning : [lower, upper, n_points]
+        """
+        self.ref_values = tf.convert_to_tensor(values, dtype=tf.float32)
+        self.lower, self.upper, self.n_points = binning
+    
+    def __call__(self, *args, **kwargs):
+        return self.interp(*args, **kwargs)
+    
+    def interp(self, points):
+        """
+        points : tensor-like with shape (N,), as the points to be interpolated.
+        
+        return : linearly interpolated values at the points with shape (N,). Extration is clipped by the boundary values. 
+        """
+        points = tf.convert_to_tensor(points, dtype=tf.float32)
+        return tfp.math.interp_regular_1d_grid(points, self.lower, self.upper, self.ref_values, fill_value='constant_extension')
